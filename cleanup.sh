94f34a177f17579b2b55354d39e75c148795e96d
Alard <alard@example.com>
2012-07-01 00:11:00 +0200

    Cleanup script.

---
+++ cleanup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+rm -rf /var/cache/apt/* \
+       /var/cache/debconf/* \
+       /var/lib/apt/* \
+       /var/lib/aptitude/* \
+       /var/log/* \
+       /usr/share/aptitude/* \
+       /usr/share/doc/* \
+       /usr/share/man/*
+
+find /usr/share/locale/ -mindepth 1 -maxdepth 1 -type d ! -name "en*" -exec rm -rf {} \;
+
+echo "Zero-filling /data"
+dd if=/data/zero of=/data/fill bs=10M
+rm -f /data/fill
+
+echo "Zero-filling /"
+dd if=/dev/zero of=/root/fill bs=10M
+rm -f /root/fill
+
