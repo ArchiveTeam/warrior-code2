840085f2e29b064bc29ab718c9d54f37a817c8cd
Alard <alard@example.com>
2012-08-09 10:15:25 +0200

    Upgrade the seesaw kit if necessary.

--- warrior-install.sh
+++ warrior-install.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 # This script should install/update the warrior, if necessary.
 
+# Check the seesaw-kit.
+echo "Checking for the latest seesaw kit..."
+SEESAW_VERSION=$( git ls-remote https://github.com/ArchiveTeam/seesaw-kit.git HEAD | cut -f 1 )
+if ! sudo pip freeze | grep -q $SEESAW_VERSION
+then
+  echo "Upgrading the seesaw kit..."
+  sudo pip install -e "git+https://github.com/ArchiveTeam/seesaw-kit.git#egg=seesaw"
+else
+  echo "No need to upgrade the seesaw kit."
+fi
+
