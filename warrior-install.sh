6cd0d3759de2f4a28c409003010d9d7fb4185563
Alard <alard@example.com>
2012-10-13 14:44:33 +0200

    Install development branch of seesaw-kit.

--- warrior-install.sh
+++ warrior-install.sh
@@ -3,11 +3,11 @@
 
 # Check the seesaw-kit.
 echo "Checking for the latest seesaw kit..."
-SEESAW_VERSION=$( git ls-remote https://github.com/ArchiveTeam/seesaw-kit.git HEAD | cut -f 1 )
+SEESAW_VERSION=$( git ls-remote https://github.com/ArchiveTeam/seesaw-kit.git development | cut -f 1 )
 if ! sudo pip freeze | grep -q $SEESAW_VERSION
 then
   echo "Upgrading the seesaw kit..."
-  sudo pip install -e "git+https://github.com/ArchiveTeam/seesaw-kit.git#egg=seesaw"
+  sudo pip install -e "git+https://github.com/ArchiveTeam/seesaw-kit.git@development#egg=seesaw"
 else
   echo "No need to upgrade the seesaw kit."
 fi
