3ce91ce34d801886935ade6d570056365e1033e5
David Triendl <david@triendl.name>
2013-02-25 22:11:59 +0100

    make-data-disk: Use root partition if there is no suitable data disk

--- make-data-disk.sh
+++ make-data-disk.sh
@@ -22,14 +22,21 @@ do
   fi
 done
 
-# reset partition table
-echo "Creating a data partition on ${data_disk}..."
-( echo d ; echo d ; echo d ; echo d ; echo d ; echo n ; echo p ; echo 1 ; echo ; echo ; echo w ) | fdisk $data_disk &> /dev/null
+if [ -z "$data_disk" ]
+then
+  echo "No suitable device found for data disk"
+  rm -rf /data/data
+else
+  # reset partition table
+  echo "Creating a data partition on ${data_disk}..."
+  ( echo d ; echo d ; echo d ; echo d ; echo d ; echo n ; echo p ; echo 1 ; echo ; echo ; echo w ) | fdisk $data_disk &> /dev/null
 
-# format drive, mount and prepare folders
-echo "Preparing the data partition..."
-mke2fs -t ext4 -O ^has_journal -E lazy_itable_init=1 ${data_disk}1 &> /dev/null
-mount -t ext4 -o noatime,nodiratime,data=writeback,barrier=0,nobh ${data_disk}1 /data
+  # format drive, mount and prepare folders
+  echo "Preparing the data partition..."
+  mke2fs -t ext4 -O ^has_journal -E lazy_itable_init=1 ${data_disk}1 &> /dev/null
+  mount -t ext4 -o noatime,nodiratime,data=writeback,barrier=0,nobh ${data_disk}1 /data
+fi
+
 mkdir /data/data
 chmod 777 /data /data/data
 
