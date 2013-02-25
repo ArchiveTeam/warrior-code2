5416942c7c1dbfdd014983cd604a36f2ec1b4738
David Triendl <david@triendl.name>
2013-02-25 08:18:20 +0100

    warrior-runner: Try to fetch config from EC2 userdata
    If the config file does not exist, try to fetch it from Amazon EC2 userdata.

--- warrior-runner.sh
+++ warrior-runner.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+PROJECTS_DIR="/home/warrior/projects"
+DATA_DIR="/data/data"
+CODE_DIR="`dirname $0`"
+
 while [ ! -f /dev/shm/ready-for-warrior ]
 do
   sleep 1
 done
 
-cd /home/warrior/warrior-code2
+if [ ! -f "$PROJECTS_DIR/config.json" ]
+then
+  wget -T 2 -t 0 -q -O "$PROJECTS_DIR/config.json" http://169.254.169.254/latest/user-data
+fi
+
+cd "$CODE_DIR"
 
 run-warrior \
-  --projects-dir /home/warrior/projects \
-  --data-dir /data/data \
+  --projects-dir "$PROJECTS_DIR" \
+  --data-dir "$DATA_DIR" \
   --warrior-hq http://warriorhq.archiveteam.org \
   --port 8001 \
   --real-shutdown
