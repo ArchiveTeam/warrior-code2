bf8ca88fea309b61e500686869bccec82302f523
Christopher Foo <chris.foo@gmail.com>
2014-10-29 17:22:13 -0400

    Support Python 3.

--- warrior-runner.sh
+++ warrior-runner.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+WARRIOR=run-warrior2
+
+if type python3 --version > /dev/null 2>&1; then
+  WARRIOR=run-warrior3
+fi
+
 while [ ! -f /dev/shm/ready-for-warrior ]
 do
   sleep 1
@@ -7,7 +13,7 @@ done
 
 cd /home/warrior/warrior-code2
 
-run-warrior \
+$WARRIOR \
   --projects-dir /home/warrior/projects \
   --data-dir /data/data \
   --warrior-hq http://warriorhq.archiveteam.org \
